a5907f2f5d53a93c2b46373d76c07b9600ea6bab
Sidik0001 <sidikiba.diane01@law.ac.uk>
2026-02-02 14:24:01 +0000

    Add files via upload

---
+++ Unit5/MyProfile.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My Professional Profile </title>
+</head>
+<body style='background-color:lemonchiffon';>
+
+<h1 style='font-family:Old Bookman Style;'>My Profile </h1>
+<hr/>
+<p style='font-size:110%;'>I am an international student at the University of Law, studying a Master (conversion) course in Computer Science, expected to graduate in June next year. </p>
+
+<img src='download.png' width='32' height="32">
+<a href="https://www.w3schools.com"> Here is a link to my LinkedIn Profile</a>
+
+<?php
+$t= 'Hello World!';
+echo "Seeing this text implies that you have been granted access to the backside of the page. Thus, $t";
+?>
+
+<h2>Skills and Experience</h2>
+<hr/>
+<p style='font-size:110%;'> I have some prior experience using some programming languages, primarily in data science and research aims. However, my background in Economics and Finance has also greatly shaped by understanding and interpretation of data and problem solving skills.</p>
+
+
+
+<p> </p>
+
+<h2 style='font-family:Old Bookman Style;'> Education </h2>
+<hr/>
+
+
+<h2> Hobbies </h2>
+<hr>
+<li style='font-size:110%';> List of hobbies </li>
+<hr/>
+
+</body>
+</html>
---
+++ Unit5/student_records.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 02, 2026 at 02:48 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newwork`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_records`
+--
+
+DROP TABLE IF EXISTS `student_records`;
+CREATE TABLE IF NOT EXISTS `student_records` (
+  `First Name` varchar(255) NOT NULL,
+  `Last Name` varchar(255) NOT NULL,
+  `Student ID` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student_records`
+--
+
+INSERT INTO `student_records` (`First Name`, `Last Name`, `Student ID`) VALUES
+('SIDIKIBA', 'DIANE', 12);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
